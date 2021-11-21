5143322037c552026293f100601af8fb0942909a
Paul Sanford <me@paulsanford.net>
2021-11-21 00:09:04 -0800

    Data dir and filenames

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use std::{fs::File, io::Write, path::PathBuf};
 
 use anyhow::{Result, bail};
 use bzip2::{Compression, write::BzEncoder};
+use chrono::{DateTime, Local};
+use directories::BaseDirs;
 use git2::{Repository, Status};
 use tar::Builder;
 
 fn main() -> Result<()> {
+    let bd = BaseDirs::new().unwrap();
+    let mut data_dir = PathBuf::from(bd.data_local_dir());
+    data_dir.push("hateor");
+    std::fs::create_dir_all(&data_dir)?;
     let repo = Repository::discover(".")?;
     let basepath = repo.workdir();
 
@@ -39,10 +45,19 @@ fn main() -> Result<()> {
     encoder.write_all(&tar_back)?;
 
     encoder.finish()?;
-    
-    let mut file = File::create("untracked.tar.bz2")?;
+
+    let dt: DateTime<Local> = Local::now();
+    let fname = dt.format("%Y-%m-%dT%H%M%S.tar.bz2").to_string();
+    let mut savepath = data_dir.clone();
+    savepath.push(fname);
+
+    let mut file = File::create(savepath)?;
 
     file.write_all(&bz2_back)?;
+
+    for file in new.iter() {
+        std::fs::remove_file(file.path().unwrap())?;
+    }
 
     Ok(())
 }
