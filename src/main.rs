4a1b2963120f9cd9abcfdfe1e86ee4e577e6f77f
Paul Sanford <me@paulsanford.net>
2021-11-21 19:17:30 -0800

    restoration

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,77 @@
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{env, fs::File, io::Write, path::PathBuf};
 
-use anyhow::{Result, bail};
-use bzip2::{Compression, write::BzEncoder};
+use anyhow::{bail, Result};
+use bzip2::{read::BzDecoder, write::BzEncoder, Compression};
 use chrono::{DateTime, Local};
 use directories::BaseDirs;
 use git2::{Repository, Status};
-use tar::Builder;
+use tar::{Archive, Builder};
 
-fn main() -> Result<()> {
+struct Repo {
+    repo: Repository,
+    path: PathBuf,
+}
+
+fn storage_location() -> Result<PathBuf> {
     let bd = BaseDirs::new().unwrap();
     let mut data_dir = PathBuf::from(bd.data_local_dir());
     data_dir.push("hateor");
     std::fs::create_dir_all(&data_dir)?;
+    Ok(data_dir)
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        bail!("Need a command");
+    }
+    if args[1] == "store" {
+        store()?;
+    }
+    if args[1] == "restore" {
+        if args.len() < 3 {
+            bail!("Need a filename");
+        }
+        restore(args[2].clone())?;
+    }
+
+    Ok(())
+}
+
+fn restore(file_name: String) -> Result<()> {
+    let data_dir = storage_location()?;
+    let repo = find_repo()?;
+
+    println!("Restoring from {}", file_name);
+    let file = File::open(file_name)?;
+    let decoder = BzDecoder::new(file);
+    let mut tar = Archive::new(decoder);
+
+    let non_current: Vec<_> = repo
+        .repo
+        .statuses(None)?
+        .iter()
+        .filter(|s| s.status() != Status::CURRENT && s.path().is_some())
+        .map(|s| PathBuf::from(s.path().unwrap()))
+        .collect();
+
+    println!("Tar loaded");
+
+    for entry in tar.entries()? {
+        let entry = entry?;
+        let path = entry.path()?;
+        if non_current.contains(&PathBuf::from(&*path)) {
+            println!("{:#?} already in repo and dirty", path);
+            continue;
+        }
+        let mut dest = File::create(path)?;
+        dest.write_all(&entry.path_bytes())?;
+    }
+
+    Ok(())
+}
+
+fn find_repo() -> Result<Repo> {
     let repo = Repository::discover(".")?;
     let basepath = repo.workdir();
 
@@ -19,21 +79,45 @@ fn main() -> Result<()> {
         bail!("Missing a work dir path. Is this a bare repo?");
     }
 
-    let basepath = basepath.unwrap();
+    let path = PathBuf::from(basepath.unwrap());
 
-    let statuses = repo.statuses(None)?;
-    
-    let new: Vec<_> = statuses.iter().filter(|s| s.status() == Status::WT_NEW && s.path().is_some()).collect();
+    Ok(Repo { repo, path })
+}
+
+fn store() -> Result<()> {
+    let data_dir = storage_location()?;
+    let repo = find_repo()?;
+
+    let statuses = repo.repo.statuses(None)?;
+
+    let new: Vec<_> = statuses
+        .iter()
+        .filter(|s| s.status() == Status::WT_NEW && s.path().is_some())
+        .collect();
 
     let mut tar_back: Vec<u8> = Vec::new();
+
+    let mut stored_files = Vec::new();
 
     {
         let mut tar = Builder::new(&mut tar_back);
 
-        for file in new.iter() {
-            let mut fullpath = PathBuf::from(basepath);
+        for file in new.into_iter() {
+            let mut fullpath = repo.path.clone();
             fullpath.push(file.path().unwrap());
+            {
+                let f = File::open(&fullpath)?;
+                let size = f.metadata()?.len();
+                if size > 1024 * 1024 {
+                    println!(
+                        "File {} is greater than 1mb, skipping",
+                        file.path().unwrap()
+                    );
+                    continue;
+                }
+            }
             tar.append_path_with_name(fullpath, file.path().unwrap())?;
+            stored_files.push(file);
         }
 
         tar.finish()?;
@@ -55,7 +139,7 @@ fn main() -> Result<()> {
 
     file.write_all(&bz2_back)?;
 
-    for file in new.iter() {
+    for file in stored_files.into_iter() {
         std::fs::remove_file(file.path().unwrap())?;
     }
 
